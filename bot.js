bae49b6f4cc3897bd19696635d0d63719adc9903
R@MY <mccannyoung@users.noreply.github.com>
2021-06-09 17:51:09 -0500

    Revert "Update bot.js"
    This reverts commit 99716ffd7683cd846823fe48d63a4a578b0cb9c3.

--- bot.js
+++ bot.js
@@ -31,31 +31,26 @@ client.on('ready', ()=>{
         client.channels.fetch(channel2Notify).then((channel) =>{
             channel.send(`@everyone T-Minus ~24 hours until game time! ${kuma} `);
         }
-        ).then(()=>{
-            client.destroy();    
-        });
+        );
     } 
     else if (dayOfWeek === weekday[3])
     {
-        client.channels.fetch(channel2Notify).then((channel) =>{
-            channel.send(`@everyone ${kuma} IT'S ALMOST GAME TIME!`);
-
-            var ppl2notify = process.env.userlist;
+        var ppl2notify = process.env.userlist;
    
-            if(ppl2notify.length>1) {
-                var pplList = ppl2notify.split(',');
-    
-                pplList.forEach(person =>{
-                    console.log(`person : ${person}`);
-                    client.users.fetch(person).then( (result) =>{
-                        result.send(`Game Day! ${kuma}`);
-                    }
-                );
-                });
-            }
+        if(ppl2notify.length>1) {
+            var pplList = ppl2notify.split(',');
+
+            pplList.forEach(person =>{
+                console.log(`person : ${person}`);
+                client.users.fetch(person).then( (result) =>{
+                    result.send(`Game Day! ${kuma}`);
+                }
+            );
+            });
         }
-        ).then(()=> {
-        client.destroy();
+   
+        client.channels.fetch(channel2Notify).then((channel) =>{
+            channel.send(`@everyone ${kuma} T-MINUS 15 MINUTES TIL GAME TIME!`);
         }
         );
     } else {
